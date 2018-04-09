c8c7a5f7d1887988bfce67388c22197fddf76142
Daniela Sainz <dasainz18@gmail.com>
2018-04-09 16:58:54 -0400

    Initial Commit

--- src/com/Sainz/Organization.java
+++ src/com/Sainz/Organization.java
@@ -1,13 +1,22 @@
 package com.Sainz;
 
+import java.util.ArrayList;
+
 public class Organization {
     private String organizationName;
+    //Included the Array List below first
+    private ArrayList<Employee> theEmployees;
 
-    public Organization(String organizationName) {
-        this.organizationName = organizationName;
+    //default constructor
+    public Organization()
+    {
+        theEmployees = new ArrayList<>();
     }
 
-    public Organization(){
+    //overloaded constructor
+    public Organization(String organizationName) {
+        this.organizationName = organizationName;
+        theEmployees = new ArrayList<>();
     }
 
     public String getOrganizationName() {
@@ -16,5 +25,17 @@ public class Organization {
 
     public void setOrganizationName(String organizationName) {
         this.organizationName = organizationName;
+    }
+    public ArrayList<Employee> getTheEmployees() {
+        return theEmployees;
+    }
+
+    public void setTheEmployees(ArrayList<Employee> theEmployees) {
+        this.theEmployees = theEmployees;
+}
+
+    public void addEmployee(Employee employee)
+    {
+        theEmployees.add(employee);
     }
 }
